d7b7f1b27a7258fcb028ad121d426054bb167e05
Ramu-Narsinga <ramu199611@gmail.com>
2018-12-16 23:21:23 +0530

    bug in logic functionality and final set up test

--- controllers/userCredentialsController.js
+++ controllers/userCredentialsController.js
@@ -66,50 +66,53 @@ exports.user_signup = function(req, res) {
 exports.user_login = function(req, res) {
   var adminMail = "admin@ycm.in";
   var adminPwd = "Navneet@94";
+  var loggedInState = false;
 
   console.log("in login usercredentials controller, req.body check", req.body);
   userCredentialsModel.findOne({
-      email: req.body.email
-    }, function(err, loginres) {
-      if (err) {
-        //console.log("err" + err);
+    email: req.body.email
+  }, function(err, loginres) {
+    if (err) {
+      console.log("err" + err);
+    } else {
+      console.log("loginres", loginres);
+      console.log("what's in admin mail and pwd before if", adminMail, adminPwd);
+      if (req.body.email == adminMail && req.body.password == adminPwd) {
+        console.log("what's in admin mail and pwd", adminMail, adminPwd);
+        req.session.user = req.body.email;
+        req.session.role = 'admin';
+        res.json({
+          result: 'success',
+          role: 'admin',
+          message: 'You have successfully logged in as admin'
+        });
+        loggedInState = true;
       } else {
-        console.log("loginres", loginres);
-        var loggedInState = false;
         if (loginres != null) {
-          if (req.body.email == adminMail && req.body.password == adminPwd) {
-            req.session.user = req.body.email;
-            req.session.role = 'admin';
+          if (req.body.email.length == 0 || req.body.password.length == 0) {
             res.json({
+              Msg: "Please provide valid login details"
+            })
+          } else {
+            req.session.user = req.body.email;
+            req.session.role = 'generic'
+            res.json({
+              loginres,
               result: 'success',
-              role: 'admin',
-              message: 'You have successfully logged in as admin'
+              role: 'generic',
+              message: 'You have successfully logged in'
             });
             loggedInState = true;
-          } else {
-            if (req.body.email.length == 0 || req.body.password.length == 0) {
-              res.json({
-                Msg: "Please provide valid login details"
-              })
-            } else {
-              req.session.user = req.body.email;
-              req.session.role = 'generic'
-              res.json({
-                loginres,
-                result: 'success',
-                role: 'generic',
-                message: 'You have successfully logged in'
-              });
-              loggedInState = true;
-            }
           }
         }
+        console.log("what's in loggedInState before notifying", loggedInState)
+        if (!loggedInState) {
+          res.json({
+            result: "Error",
+            message: "Please provide valid Username and Password"
+          });
+        }
       }
-      if (!loggedInState) {
-        res.json({
-          result: "Error",
-          message: "Please provide valid Username and Password"
-        });
-      }
+    }
   });
 }
